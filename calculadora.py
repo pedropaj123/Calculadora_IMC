0738ea7c4ccc7a50fb3afff81d6a9cf4c7af241c
Pedro Silva <pedropaj@gmail.com>
2025-01-23 13:48:14 -0300

    Adicionando front-end

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,40 @@
-# Pegando dados de entrada
-peso = float(input('Informe seu peso(kg)? ')) # Pegando número de peso
-altura = float(input('Informe sua altura(exemplo: 1.75)? ')) # Pegando número de sua altura
+from flask import Flask, render_template, request
 
-# Calculo do IMC
-def calcular_imc(peso, altura): 
-    return peso / (altura ** 2)
+app = Flask(__name__)
 
-# Indicando variável do calculo
-imc = calcular_imc(peso, altura)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Indicando a classificação do IMC
-if imc <= 18.5:
-    classificacao = "Abaixo do normal"
-elif imc > 18.5 and imc <= 24.9:
-    classificacao = "Normal"
-elif imc > 24.9 and imc <= 29.9:
-    classificacao = "Sobrepeso"
-elif imc > 29.9 and imc <= 34.9:
-    classificacao = "Obesidade grau 1"
-elif imc > 34.9 and imc <= 39.9:
-    classificacao = "Obesidade grau 2"
-else:
-    classificacao = "Obesidade grau 3"
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    try:
+        peso = float(request.form['peso'])
+        altura = float(request.form['altura'])
+        if altura == 0  :
+            return "Erro: Altura não pode ser zero!", 400
+        
+        # Cálculo do IMC
+        imc = peso / (altura ** 2)
+        
+        # Classificação do IMC
+        if imc <= 18.5:
+            classificacao = "Abaixo do normal"
+        elif imc <= 24.9:
+            classificacao = "Normal"
+        elif imc <= 29.9:
+            classificacao = "Sobrepeso"
+        elif imc <= 34.9:
+            classificacao = "Obesidade grau 1"
+        elif imc <= 39.9:
+            classificacao = "Obesidade grau 2"
+        else:
+            classificacao = "Obesidade grau 3"
+        
+        # Renderiza a página de resultado
+        return render_template('resultado.html', imc=imc, classificacao=classificacao)
+    except Exception as e:
+        return f"Erro: {e}", 500
 
-# Indicando Resultado
-print (f"IMC:{imc:.2f}, Classificação:{classificacao}")
+if __name__ == '__main__':
+    app.run(debug=True)
